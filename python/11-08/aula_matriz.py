5ae62f248ed2cd09cedc3bf177c153bc2521d93a
samyfg41 <samanthafaruolo@gmail.com>
2025-08-11 09:28:34 -0300

    Aula Python - 11/08

---
+++ python/11-08/aula_matriz.py
@@ -0,0 +1,33 @@
+'''
+1 - Função para criar uma matriz (lista de lista)
+2 - Função que recebe a matriz númerica por parâmetro e retorna a soma de todos os elementos
+3 - Função para imprimir a soma dos elementos da matriz
+'''
+
+def criar_matriz(n_linhas, n_colunas):
+    matriz = []
+    for item in range(n_linhas):
+        linha = []
+        for j in range(n_colunas):
+            n = int(input('Números: '))
+            linha.append(n)
+        matriz.append(linha)
+    return matriz
+
+def somar_elementos(matriz):
+    soma = [] #var acumuladora
+    for linha in range(len(matriz)):
+        for coluna in range(len(matriz[linha])):
+            soma+= matriz[linha][coluna]
+    return soma
+
+def imprimir(soma):
+    print(f'Soma total: {soma}')
+
+def main():
+    matriz = criar_matriz(3, 3)
+    soma = somar_elementos(matriz)
+    imprimir(soma)
+
+#programa principal
+main()
